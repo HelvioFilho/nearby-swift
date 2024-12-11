613d0a3b6754c45d1f2ec31cb2ad975de57a9a85
Helvio Filho <helviosvf@gmail.com>
2024-12-11 11:03:38 -0300

    feat: implement navigation flow from Splash to Welcome screen
    - Added SplashFlowDelegate to manage navigation decisions from the Splash screen.
    - Updated NearbyFlowController to conform to SplashFlowDelegate, allowing navigation to WelcomeViewController.
    - Modified SplashViewController to accept a delegate and trigger navigation after a delay.
    - Enhanced navigation logic to cleanly handle transitions between Splash and Welcome screens.

---
+++ nearby/Sources/Features/Scenes/Splash/SplashFlowDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SplashFlowDelegate.swift
+//  nearby
+//
+//  Created by helvio on 11/12/24.
+//
+
+import Foundation
+
+public protocol SplashFlowDelegate: AnyObject {
+    func decideNavigationFlow()
+}
